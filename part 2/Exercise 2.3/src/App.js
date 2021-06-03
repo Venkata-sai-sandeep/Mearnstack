d4d5fb733fb73d333cece40b6188c0b62a46742a
Venkata-sai-sandeep <74000648+Venkata-sai-sandeep@users.noreply.github.com>
2021-06-03 15:41:43 +0530

    Add files via upload

---
+++ part 2/Exercise 2.3/src/App.js
@@ -0,0 +1,80 @@
+import React from 'react'
+
+const Header = ({ object }) => {
+  return (
+      <h2>{object.name}</h2>
+  )
+}
+
+const Part = (pro) => {
+  return (
+      <p>
+          {pro.part.name} {pro.part.exercises}
+      </p>
+  )
+}
+
+const Content = ({ object }) => {
+  return (
+      <div>
+          {object.parts.map((part) => <Part part={part} />)}
+      </div>
+  )
+}
+
+const Total = ({ object }) => {
+  const sum1 = object.parts.reduce((total, part) => total + part.exercises, 0)
+  return (
+      <strong>Number of exercises {sum1}</strong>
+  )
+}
+
+const Course = ({ object }) => {
+  return (
+      <>
+          <Header object={object} />
+          <Content object={object} />
+          <Total object={object} />
+      </>
+  )
+}
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    
+  ]
+
+  return (
+    <>
+      
+      {courses.map(object => <Course object={object} />)}
+    </>
+  )
+}
+
+export default App
