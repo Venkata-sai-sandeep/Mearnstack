4d458018350fd62b632d0b47b204ca8674b21a7c
Venkata-sai-sandeep <74000648+Venkata-sai-sandeep@users.noreply.github.com>
2021-06-03 18:33:21 +0530

    Add files via upload

---
+++ part 2/Exercise 2.13/src/components/Searching.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import BasicCountryInformation from './BasicCountryInformation'
+
+
+const Searching = ({ countries, newfilter,showC }) => {
+    const filtered = countries.filter(country =>
+      country.name.toUpperCase().includes(newfilter.toUpperCase())
+    );
+    const listLen = filtered.length;
+    console.log(filtered);
+    if(newfilter===''){
+          return <div />
+    }
+    else{
+
+     if (listLen >= 10) {
+      return <div>Too many matches, specify another letter(s)</div>;
+    }
+  
+    else if (listLen > 1 && listLen < 10) {
+      return filtered.map(country => (
+        <div key={country.name}>
+          {country.name}<button value={country.name} onClick={showC}>show</button>
+          
+        </div>
+      ));
+    } 
+    else {
+      return (
+        <div>
+          <BasicCountryInformation country={filtered[0]} />
+        </div>
+      );
+    }
+}
+  };
+export default Searching
